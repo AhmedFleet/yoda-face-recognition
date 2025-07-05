e7c2cce30e8a900a3d19c8f48cd4d0d30bde3545
AhmedFleet <128899328+AhmedFleet@users.noreply.github.com>
2025-07-05 03:48:05 +0200

    Delete .render-build.sh

--- .render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  # أي خطأ يوقف التنفيذ فورًا
-
-echo "🔧 Disabling build isolation for pip..."
-export PIP_NO_BUILD_ISOLATION=0
-
-echo "📦 Installing requirements..."
-pip install -r requirements.txt
