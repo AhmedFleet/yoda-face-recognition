4fed006da3ea475aac6598722981452933cefc74
AhmedFleet <128899328+AhmedFleet@users.noreply.github.com>
2025-07-05 03:00:42 +0200

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,2 +1,7 @@
-#!/usr/bin/env bash
-echo "pythonVersion=3.10" > .python-version
+#!/bin/bash
+
+# تحديد إصدار بايثون المطلوب
+echo "python-3.10" > runtime.txt
+
+# تعطيل العزل أثناء البناء لحل مشكلة Pillow
+PIP_NO_BUILD_ISOLATION=0 pip install -r requirements.txt
